079cf0076c20e162ffaf421cfd96305cbe7c3b71
fariz tiger <tracecoin.io@gmail.com>
2021-10-08 13:44:16 +0800

    g

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -50,16 +50,16 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'IdleSpace'`, () => {
+  // it(`should have as title 'Polybeesminer'`, () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('IdleSpace');
+  //   expect(app.title).toEqual('Polybeesminer');
   // });
 
   // it('should render title in a h1 tag', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to IdleSpace!');
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to Polybeesminer!');
   // });
 });
